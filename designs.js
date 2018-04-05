e49f5a803b5c3faa65eb454d3d05b4b436e7cc44
Wenhan <wenhan.work@gmail.com>
2018-04-05 13:05:40 -0600

    makeGrid now takes the value changes of height and width

--- designs.js
+++ designs.js
@@ -2,9 +2,20 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+var gridHeight = 1;
+var gridWidth = 1;
+
+$('#submit_button').click(makeGrid);
+$('#inputHeight').on('change paste keyup',function() {
+    gridHeight = $('#inputHeight').val();
+});
+$('#inputWidth').on('change paste keyup',function() {
+    gridWidth = $('#inputWidth').val();
+})
 
 function makeGrid() {
-
-// Your code goes here!
-
+    
+    console.log(gridHeight);
+    $('h1').text(gridHeight);
+    console.log($('h1').text());
 }
