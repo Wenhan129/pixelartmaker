11c7540062fe92fa9901f070909018a1267571d5
Wenhan <wenhan.work@gmail.com>
2018-04-08 22:09:25 -0600

    All work done.

--- script.js
+++ script.js
@@ -2,58 +2,65 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-var gridHeight = 1;
-var gridWidth = 1;
-var gridColor = "black";
+let gridHeight = 1;
+let gridWidth = 1;
+let gridColor = "black";
 
+// If the height and width changed, update the global value
 $('#inputHeight').on('change paste keyup',function() {
     gridHeight = $('#inputHeight').val();
     limitControl($(this));
 });
+
 $('#inputWeight').on('change paste keyup',function() {
     gridWidth = $('#inputWeight').val();
     limitControl($(this));
 });
 
+// If the color picked, update the color value
 colorPicker.addEventListener("change", function() {
     gridColor = this.value;
 })
 
-
-$(document).ready(function(){
+// Submit button function
+$(document).ready(function() {
     $('#submit_button').click(function(e){        
         e.preventDefault();
         makeGrid();        
     });
 });
 
-function makeGrid() {
-    // location.reload();
-    generateTable();   
-    
+// To pre-judge if the input value is too much or not
+function limitControl(input) {
+    if ((input.val() > 100)||(input.val() < 0)) {
+        input.val(1);
+        alert("Please input a number between 0 and 100.");   
+}
 }
 
-function generateTable() {
+// A DOM method to generate a table
+// As a child class of table id: pixelCanvas
+function makeGrid() {
     // Clear the table generated last time
     document.getElementById("pixelCanvas").innerHTML = "";
     // get the reference for the body
-    var body = document.getElementsByTagName("body")[0];
+    let body = document.getElementsByTagName("body")[0];
    
     // creates a <table> element and a <tbody> element
-    var tbl = document.createElement("table");
+    let tbl = document.createElement("table");
     tbl.className = "gridTable";
-    var tblBody = document.createElement("tbody");
+    let tblBody = document.createElement("tbody");
    
     // creating all cells
-    for (var i = 0; i < gridHeight; i++) {
+    for (let i = 0; i < gridHeight; i++) {
       // creates a table row
-      var row = document.createElement("tr");
+      let row = document.createElement("tr");
    
-      for (var j = 0; j < gridWidth; j++) {
+      for (let j = 0; j < gridWidth; j++) {
         // Create a <td> element and a text node, make the text
         // node the contents of the <td>, and put the <td> at
         // the end of the table row
-        var cell = document.createElement("td");
+        let cell = document.createElement("td");
         
         cell.addEventListener("click", function() {
             this.style.backgroundColor = gridColor;
@@ -74,9 +81,4 @@ function generateTable() {
     tbl.setAttribute("border", "2");
 
     document.getElementById("pixelCanvas").appendChild(tbl); 
-}
-
-function limitControl(input) {
-    if ((input.val() > 100)||(input.val() < 0))
-        input.val(1);
 }
